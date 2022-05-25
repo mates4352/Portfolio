d4b209ad083a6f218d26fd5add9e0d70f5f27fbc
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-05-25 14:30:59 +0300

    Add component About

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import s from './App.module.scss';
+import {About} from "./pages/about/About";
 
 type appType = {
 
 }
 
 export const App:React.FC<appType> = () => {
+  const classTheme = true ? s.light_theme : s.dark_theme;
   return (
-    <div className={s.App}>
+    <div className={`${s.app} ${classTheme}`}>
+      <About/>
     </div>
   );
 }
---
+++ src/pages/about/About.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import s from './About.module.scss';
+import {IconArrow} from "../../shared/icons/IconArrow";
+
+type aboutType = {
+
+};
+
+export const About:React.FC<aboutType> = (props) => {
+   const {} = props;
+
+   return (
+       <section className={`${s.about} ${s.container}`}>
+            <div className={s.image}></div>
+
+            <div className={s.info}>
+               <h2 className={s.title}>
+                  I'M Sergey Saburkin.<b>WEB DEVELOPER</b>
+               </h2>
+
+               <p className={s.text}>
+                  Good day! My name is Sergey for more than half a year, I have been
+                  creating websites of various levels of complexity,
+                  I began to study vertska in the summer of 2020, during which time
+                  I acquired enough knowledge to perform complex project.
+               </p>
+
+               <a className={s.button} href='/'>
+                  <span>More about me</span>
+                  <div className={s.button_icon}>
+                     <IconArrow/>
+                  </div>
+               </a>
+            </div>
+       </section>
+   );
+};
