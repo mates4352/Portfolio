e02972ed394ad0e0a8493b4d74be5fd1571a3fee
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-05-26 16:34:50 +0300

    Add component Info

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './About.module.scss';
 import {BlockAnimation} from "../../shared/interactive/blockAnimation/BlockAnimation";
-import {Button} from "../../shared/interactive/button/Button";
+import {Info} from "./components/info/Info";
 
 type aboutType = {
 
@@ -17,89 +17,10 @@ export const About:React.FC<aboutType> = (props) => {
           <div className={s.container}>
              <hgroup className={s.title_wrap}>
                 <h1 className={s.title}>SUMMARY</h1>
-                <h2 className={s.subTitle}>ABOUT ME</h2>
+                <h2 className={s.subTitle}>ABOUT <span>ME</span></h2>
              </hgroup>
 
-             <section className={s.info}>
-                <div className={s.info_wrap}>
-                   <h2 className={s.info_title}>PERSONAL INFORMATION</h2>
-
-                   <dl className={s.info_list}>
-                      <div className={s.info_wrap}>
-                         <dt className={s.info_key}>Name:</dt>
-                         <dd className={s.info_caption}>Sergey</dd>
-                      </div>
-
-                      <div className={s.info_wrap}>
-                         <dt className={s.info_key}>Telephone:</dt>
-                         <dd className={s.info_caption}>+7 908 907 73 78</dd>
-                      </div>
-
-                      <div className={s.info_wrap}>
-                         <dt className={s.info_key}>Surname:</dt>
-                         <dd className={s.info_caption}>Saburkin</dd>
-                      </div>
-
-                      <div className={s.info_wrap}>
-                         <dt className={s.info_key}>Mail:</dt>
-                         <dd className={s.info_caption}>sergeysabyrkin@gmail.com</dd>
-                      </div>
-
-                      <div className={s.info_wrap}>
-                         <dt className={s.info_key}>Age:</dt>
-                         <dd className={s.info_caption}>23</dd>
-                      </div>
-
-                      <div className={s.info_wrap}>
-                         <dt className={s.info_key}>Telegram:</dt>
-                         <dd className={s.info_caption}>@SergeySabyrkin</dd>
-                      </div>
-
-                      <div className={s.info_wrap}>
-                         <dt className={s.info_key}>Nationality:</dt>
-                         <dd className={s.info_caption}>Russia</dd>
-                      </div>
-
-                      <div className={s.info_wrap}>
-                         <dt className={s.info_key}>freelancer:</dt>
-                         <dd className={s.info_caption}>There is experience</dd>
-                      </div>
-                   </dl>
-
-                   <Button href={'https://drive.google.com/file/d/1TMa0VFhyPhFT3m-Sbb5ZeD3ai6xhAu4Y/view?usp=sharing'}>DOWNLOAD CV</Button>
-                </div>
-
-
-                <ul className={s.info_list_cards}>
-                   <li className={s.info_item_card}>
-                      <article className={s.info_card}>
-                         <h2 className={s.info_card_title}>2</h2>
-                         <p className={s.info_card_text}>YEAR OF EXPERIENCE</p>
-                      </article>
-                   </li>
-
-                   <li className={s.info_item_card}>
-                      <article className={s.info_card}>
-                         <h2 className={s.info_card_title}>50</h2>
-                         <p className={s.info_card_text}>COMPLETED PROJECTS</p>
-                      </article>
-                   </li>
-
-                   <li className={s.info_item_card}>
-                      <article className={s.info_card}>
-                         <h2 className={s.info_card_title}>2</h2>
-                         <p className={s.info_card_text}>COMPLETED BIG PROJECTS</p>
-                      </article>
-                   </li>
-
-                   <li className={s.info_item_card}>
-                      <article className={s.info_card}>
-                         <h2 className={s.info_card_title}>2</h2>
-                         <p className={s.info_card_text}>COMPLETED COURSE</p>
-                      </article>
-                   </li>
-                </ul>
-             </section>
+             <Info/>
 
              <section className={s.skills}>
                 <h2 className={s.skills_title}>MY SKILLS</h2>
---
+++ src/pages/about/components/info/Info.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import s from './Info.module.scss';
+import {Button} from "../../../../shared/interactive/button/Button";
+
+type infoType = {
+   
+};
+
+export const Info:React.FC<infoType> = (props) => {
+   const {} = props;
+
+   return (
+       <section className={s.info}>
+          <div className={s.wrap}>
+             <h2 className={s.title}>PERSONAL INFORMATION</h2>
+
+             <dl className={s.list}>
+                <div className={s.list_item}>
+                   <dt className={s.key}>Name:</dt>
+                   <dd className={s.caption}>Sergey</dd>
+                </div>
+
+                <div className={s.list_item}>
+                   <dt className={s.key}>Telephone:</dt>
+                   <dd className={s.caption}>+7 908 907 73 78</dd>
+                </div>
+
+                <div className={s.list_item}>
+                   <dt className={s.key}>Surname:</dt>
+                   <dd className={s.caption}>Saburkin</dd>
+                </div>
+
+                <div className={s.list_item}>
+                   <dt className={s.key}>Mail:</dt>
+                   <dd className={s.caption}>sergeysabyrkin@gmail.com</dd>
+                </div>
+
+                <div className={s.list_item}>
+                   <dt className={s.key}>Age:</dt>
+                   <dd className={s.caption}>23</dd>
+                </div>
+
+                <div className={s.list_item}>
+                   <dt className={s.key}>Telegram:</dt>
+                   <dd className={s.caption}>@SergeySabyrkin</dd>
+                </div>
+
+                <div className={s.list_item}>
+                   <dt className={s.key}>Nationality:</dt>
+                   <dd className={s.caption}>Russia</dd>
+                </div>
+
+                <div className={s.list_item}>
+                   <dt className={s.key}>freelancer:</dt>
+                   <dd className={s.caption}>There is experience</dd>
+                </div>
+             </dl>
+
+             <Button href={'https://drive.google.com/file/d/1TMa0VFhyPhFT3m-Sbb5ZeD3ai6xhAu4Y/view?usp=sharing'}>DOWNLOAD CV</Button>
+          </div>
+
+
+          <ul className={s.sublist}>
+             <li className={s.sublist_item}>
+                <article className={s.card}>
+                   <h2 className={s.card_title}>2</h2>
+                   <p className={s.card_text}>YEAR OF EXPERIENCE</p>
+                </article>
+             </li>
+
+             <li className={s.sublist_item}>
+                <article className={s.card}>
+                   <h2 className={s.card_title}>50</h2>
+                   <p className={s.card_text}>COMPLETED PROJECTS</p>
+                </article>
+             </li>
+
+             <li className={s.sublist_item}>
+                <article className={s.card}>
+                   <h2 className={s.card_title}>2</h2>
+                   <p className={s.card_text}>COMPLETED BIG PROJECTS</p>
+                </article>
+             </li>
+
+             <li className={s.sublist_item}>
+                <article className={s.card}>
+                   <h2 className={s.card_title}>2</h2>
+                   <p className={s.card_text}>COMPLETED COURSE</p>
+                </article>
+             </li>
+          </ul>
+       </section>
+   );
+};
